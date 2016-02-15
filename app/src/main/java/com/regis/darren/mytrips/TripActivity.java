3ee10619018d0d6536e44deaab1e674e1261eb92
darrenZgreat <schlagerd@live.com>
2016-02-15 12:13:42 -0700

    Updated Trip layout to use buttons for the date

--- app/src/main/java/com/regis/darren/mytrips/TripActivity.java
+++ app/src/main/java/com/regis/darren/mytrips/TripActivity.java
@@ -3,6 +3,7 @@ package com.regis.darren.mytrips;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,10 +18,10 @@ public class TripActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         EditText tripNameField = (EditText) findViewById(R.id.tripName);
-        EditText startDateField = (EditText) findViewById(R.id.startDate);
-        EditText endDateField = (EditText) findViewById(R.id.endDate);
+        Button startDateField = (Button) findViewById(R.id.startDate);
+        Button endDateField = (Button) findViewById(R.id.endDate);
 
-        Trip trip = (Trip)intent.getSerializableExtra("trip");
+        Trip trip = (Trip) intent.getSerializableExtra("trip");
         tripNameField.setText(trip.getName());
         startDateField.setText(trip.getStartDate());
         endDateField.setText(trip.getEndDate());
